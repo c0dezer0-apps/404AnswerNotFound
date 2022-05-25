15c540048ca2fa4d4df32199e6e89933a864acd6
Brian Blankenship <c0dezer019@zohomail.com>
2022-05-25 16:24:39 -0500

    Renamed

--- migrations/20220519053725-create-problem.js
+++ migrations/20220519053725-create-problem.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('problems', {
+    await queryInterface.createTable('Problems', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -43,6 +43,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('problems');
+    await queryInterface.dropTable('Problems');
   }
 };
